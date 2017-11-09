bed61e9b60f8a3e6bc370dc26bc7e509059beffe
banifest <banifest@gmail.com>
2017-11-10 00:37:10 +0300

    create "add desease" procedure

--- insert_procedure.sql
+++ insert_procedure.sql
@@ -130,3 +130,60 @@ AS
 		@main_doctor
 	)
 GO
+	CREATE PROCEDURE dbo.add_examination 
+    @doctor_id int,
+	@patient_id int,
+	@param xml
+AS
+	INSERT INTO hospital.dbo.Examinations
+	(
+		doctor_id,
+		patient_id,
+		param
+	)
+	values
+	(
+		@doctor_id,
+		@patient_id,
+		@param
+	)
+GO
+	CREATE PROCEDURE dbo.add_disease 
+    @name_disease nvarchar(50),
+	@begin_date date,
+	@end_date date,
+	@doctor_id int,
+	@patient_id int
+AS
+	INSERT INTO hospital.dbo.Diseases
+	(
+		name_disease,
+		begin_date,
+		end_date,
+		doctor_id,
+		patient_id
+	)
+	values
+	(
+		@doctor_id,
+		@begin_date,
+		@end_date,
+		@doctor_id,
+		@patient_id
+	)
+GO
+	CREATE PROCEDURE dbo.add_drags_to_disease 
+    @disease_id int,
+	@drag_id int
+AS
+	INSERT INTO hospital.dbo.DiseasesDrags
+	(
+		disease_id,
+		drag_id
+	)
+	values
+	(
+		@disease_id,
+		@drag_id
+	)
+GO
