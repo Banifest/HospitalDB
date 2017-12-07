864b67a1e462145128d9b53b7484d95b1f43dd9f
banifest <banifest@gmail.com>
2017-12-07 21:12:44 +0300

    добавлен ввод и вывод

---
+++ auto_gen_script.py
@@ -0,0 +1,40 @@
+import random
+
+from client.model.dbConnect import connection_to_db
+
+conn = connection_to_db('test', '123')
+cursor = conn.cursor()
+MAN_NAMES = ["Алексанлдр", "Алексей", "Владислав", "Владимир", "Петр", "Василий"]
+WOMAN_NAMES = ["Оксана", "Ольга", "Татьяна", "Ксения", "Екатерина", "Елизавета"]
+
+SURNAMES = ["Петров", "Сидоров", "Смирнов", "Иванов", "Сычевский", "Бударевский"]
+
+MAN_FIO = ["{0} {1}".format(random.choice(MAN_NAMES), random.choice(SURNAMES)) for x in range(0, 40000)]
+
+WOMAN_FIO = ["{0} {1}".format(random.choice(WOMAN_NAMES), random.choice(SURNAMES)) for x in range(0, 40000)]
+
+file = open('script.sql', "w")
+counter = 10000
+for x in MAN_FIO:
+    counter += 1
+    cursor.execute("""EXEC [add_patient] '{0}', 1, '{1}', '{2}', '{3}', '{4}';""".format(
+        x, 12345, str(counter) + x, 123, '01-01-{2}'.format(
+            random.randint(1, 28), random.randint(1, 12), random.randint(1900, 2017)
+        )
+    ))
+
+for x in WOMAN_FIO:
+    counter += 1
+    cursor.execute("""EXEC [add_patient] '{0}', 0, '{1}', '{2}', '{3}', '{4}';""".format(
+        x, 220000, str(counter) + x, 123, '01-01-{2}'.format(
+            random.randint(1, 28), random.randint(1, 12), random.randint(1900, 2017)
+        )
+    ))
+#
+# for x in range(100):
+#     file.write("""EXEC [add_doctor] '{0}', '{1}', '{2}', '{3}', '{4}';\n
+#       GO\n""".format(
+#         'doctor' + str(x), 220000, str(counter) + x, 123, '{0}-{1}-{2}'.format(
+#             random.randint(1, 28), random.randint(1, 12), random.randint(1900, 2017)
+#         )
+#     ))
